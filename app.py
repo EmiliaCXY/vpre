696e028c0fe6bf9108edf9e30710496a4cbc0364
EmiliaCXY <44874323+EmiliaCXY@users.noreply.github.com>
2020-05-16 16:09:23 -0700

    can take user input and linked front end to python

--- app.py
+++ app.py
@@ -1,16 +1,34 @@
 from flask import Flask, request, jsonify, render_template
 
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
 @app.route('/predict',methods=['POST'])
-def tired():
+def predict():
     #some computations
     #load our model and get the output
-    return render_template('index.html', prediction_text='yes'.format(output))
+    # with open("ref.csv", 'r', newline='') as f:
+    #     reader = csv.reader(f)
+    #     for row in reader:
+    #         output = row[0]
+    #
+    # count = 0
+    # for i in output:
+    #     if count % 40 == 0:
+    #         front = output[:count]
+    #         back = output[count:]
+    #         output = front + "\n" + back
+    #     count += 1
+    output = ''
+
+    for x in request.form['sequence']:
+        output = output + x
+    return render_template('index.html', prediction_text=output)
 
 if __name__ == "__main__":
     app.run(debug=True)
