a83ee0ce99110e0df21e6fb7cea43d069f263758
EmiliaCXY <44874323+EmiliaCXY@users.noreply.github.com>
2020-05-18 15:16:19 -0700

    added tensorflow

--- app.py
+++ app.py
@@ -1,8 +1,37 @@
 from flask import Flask, request, jsonify, render_template
+import numpy as np
+import tensorflow as tf
 
 
 app = Flask(__name__)
+vocab = ['A','T', 'G', 'C']# Length of the vocabulary in chars
+vocab_size = len(vocab)
 
+# The embedding dimension
+embedding_dim = 256
+
+# Number of RNN units
+rnn_units = 1024
+
+checkpoint_dir = './training_checkpoints'
+char2idx = {u:i for i, u in enumerate(vocab)}
+idx2char = np.array(vocab)
+
+def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
+  model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(vocab_size, embedding_dim,
+                              batch_input_shape=[batch_size, None]),
+    tf.keras.layers.GRU(rnn_units,
+                        return_sequences=True,
+                        stateful=True,
+                        recurrent_initializer='glorot_uniform'),
+    tf.keras.layers.Dense(vocab_size)
+  ])
+  return model
+
+new_model = build_model(vocab_size=len(vocab), embedding_dim=256, rnn_units=1024, batch_size=1)
+new_model.load_weights(tf.train.latest_checkpoint(checkpoint_dir))
+new_model.build(tf.TensorShape([1, None]))
 
 @app.route('/')
 def home():
@@ -10,25 +39,51 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    #some computations
-    #load our model and get the output
-    # with open("ref.csv", 'r', newline='') as f:
-    #     reader = csv.reader(f)
-    #     for row in reader:
-    #         output = row[0]
-    #
-    # count = 0
-    # for i in output:
-    #     if count % 40 == 0:
-    #         front = output[:count]
-    #         back = output[count:]
-    #         output = front + "\n" + back
-    #     count += 1
-    output = ''
+    output = 'A'
+    # output = generate_text(new_model, start_string=u"ATGTTTGTTTTTCTTGTTTTATTGCCACTAGTTTCTAGTCAGTGTGTT")
 
-    for x in request.form['sequence']:
-        output = output + x
     return render_template('index.html', prediction_text=output)
+
+
+def loss(labels, logits):
+  return tf.keras.losses.sparse_categorical_crossentropy(labels, logits, from_logits=True)
+
+def generate_text(model, start_string):
+  # Evaluation step (generating text using the learned model)
+
+  # Number of characters to generate
+  num_generate = 100
+
+  # Converting our start string to numbers (vectorizing)
+  input_eval = [char2idx[s] for s in start_string]
+  input_eval = tf.expand_dims(input_eval, 0)
+
+  # Empty string to store our results
+  text_generated = []
+
+  # Low temperatures results in more predictable text.
+  # Higher temperatures results in more surprising text.
+  # Experiment to find the best setting.
+  temperature = 1.0
+
+  # Here batch size == 1
+  new_model.reset_states()
+  for i in range(num_generate):
+      predictions = model(input_eval)
+      # remove the batch dimension
+      predictions = tf.squeeze(predictions, 0)
+
+      # using a categorical distribution to predict the character returned by the model
+      predictions = predictions / temperature
+      predicted_id = tf.random.categorical(predictions, num_samples=1)[-1,0].numpy()
+
+      # We pass the predicted character as the next input to the model
+      # along with the previous hidden state
+      input_eval = tf.expand_dims([predicted_id], 0)
+
+      text_generated.append(idx2char[predicted_id])
+
+  return (start_string + ''.join(text_generated))
 
 if __name__ == "__main__":
     app.run(debug=True)
